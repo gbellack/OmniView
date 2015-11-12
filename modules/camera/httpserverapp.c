3d6a77575b28dad908c9f70b03e382327e2e8bce
gbellack <gbellack@umich.edu>
2015-11-12 18:17:09 -0500

    connected tcp to camera

--- modules/camera/httpserverapp.c
+++ modules/camera/httpserverapp.c
@@ -176,6 +176,208 @@ void WebSocketHandshakeEventHandler(UINT16 uConnection)
 	g_uConnection = uConnection;
 }
 
+// Application specific status/error codes
+typedef enum{
+    // Choosing -0x7D0 to avoid overlap w/ host-driver's error codes
+    SOCKET_CREATE_ERROR = -0x7D0,
+    BIND_ERROR = SOCKET_CREATE_ERROR - 1,
+    LISTEN_ERROR = BIND_ERROR -1,
+    SOCKET_OPT_ERROR = LISTEN_ERROR -1,
+    CONNECT_ERROR = SOCKET_OPT_ERROR -1,
+    ACCEPT_ERROR = CONNECT_ERROR - 1,
+    SEND_ERROR = ACCEPT_ERROR -1,
+    RECV_ERROR = SEND_ERROR -1,
+    SOCKET_CLOSE_ERROR = RECV_ERROR -1,
+    DEVICE_NOT_IN_STATION_MODE = SOCKET_CLOSE_ERROR - 1,
+    DEVICE_NOT_IN_AP_MODE = DEVICE_NOT_IN_STATION_MODE - 1,
+    DEVICE_NOT_IN_P2P_MODE = DEVICE_NOT_IN_AP_MODE - 1,
+    STATUS_CODE_MAX = -0xBB8
+}e_AppStatusCodes;
+
+//****************************************************************************
+//
+//! \brief Opening a TCP server side socket and receiving data
+//!
+//! This function opens a TCP socket in Listen mode and waits for an incoming
+//!    TCP connection.
+//! If a socket connection is established then the function will try to read
+//!    1000 TCP packets from the connected client.
+//!
+//! \param[in] port number on which the server will be listening on
+//!
+//! \return     0 on success, -1 on error.
+//!
+//! \note   This function will wait for an incoming connection till
+//!                     one is established
+//
+//****************************************************************************
+int BsdTcpServer(unsigned short usPort)
+{
+    SlSockAddrIn_t  sAddr;
+    SlSockAddrIn_t  sLocalAddr;
+    int             iAddrSize;
+    int             iSockID;
+    int             numBytes;
+    int				numBytesTotal = 0;
+    int             iNewSockID;
+    long            lNonBlocking = 1;
+    int             picSize;
+    UINT8* 			picData;
+
+    //filling the TCP server socket address
+    sLocalAddr.sin_family = SL_AF_INET;
+    sLocalAddr.sin_port = sl_Htons((unsigned short)usPort);
+    sLocalAddr.sin_addr.s_addr = 0;
+
+    // creating a TCP socket
+    iSockID = sl_Socket(SL_AF_INET,SL_SOCK_STREAM, 0);
+    if( iSockID < 0 )
+    {
+        // error
+        ASSERT_ON_ERROR(SOCKET_CREATE_ERROR);
+    }
+
+    iAddrSize = sizeof(SlSockAddrIn_t);
+
+    // binding the TCP socket to the TCP server address
+    numBytes = sl_Bind(iSockID, (SlSockAddr_t *)&sLocalAddr, iAddrSize);
+    if( numBytes < 0 )
+    {
+        // error
+        sl_Close(iSockID);
+        ASSERT_ON_ERROR(BIND_ERROR);
+    }
+
+    // putting the socket for listening to the incoming TCP connection
+    numBytes = sl_Listen(iSockID, 0);
+    if( numBytes < 0 )
+    {
+        sl_Close(iSockID);
+        ASSERT_ON_ERROR(LISTEN_ERROR);
+    }
+
+    // setting socket option to make the socket as non blocking
+    numBytes = sl_SetSockOpt(iSockID, SL_SOL_SOCKET, SL_SO_NONBLOCKING,
+                            &lNonBlocking, sizeof(lNonBlocking));
+    if( numBytes < 0 )
+    {
+        sl_Close(iSockID);
+        ASSERT_ON_ERROR(SOCKET_OPT_ERROR);
+    }
+    iNewSockID = SL_EAGAIN;
+
+    // waiting for an incoming TCP connection
+    while( iNewSockID < 0 )
+    {
+        // accepts a connection form a TCP client, if there is any
+        // otherwise returns SL_EAGAIN
+        iNewSockID = sl_Accept(iSockID, ( struct SlSockAddr_t *)&sAddr,
+                                (SlSocklen_t*)&iAddrSize);
+        if( iNewSockID == SL_EAGAIN )
+        {
+           MAP_UtilsDelay(10000);
+        }
+        else if( iNewSockID < 0 )
+        {
+            // error
+            sl_Close(iNewSockID);
+            sl_Close(iSockID);
+            ASSERT_ON_ERROR(ACCEPT_ERROR);
+        }
+    }
+
+    // DONE WITH SOCKET INIT
+
+	InitCameraComponents(640, 480);
+
+	picSize = StartCamera((char **)&picData);
+
+	// Send picSize
+	numBytes = sl_Send(iNewSockID, (void*)&picSize, sizeof(int), 0);
+	if( numBytes < 0 ) {
+		sl_Close(iSockID);
+	    ASSERT_ON_ERROR(SEND_ERROR);
+	}
+
+	// Send pic
+	numBytesTotal = 0;
+	while(numBytesTotal != picSize) {
+		numBytes = sl_Send(iNewSockID, &picData[numBytesTotal],
+				picSize-numBytesTotal, 0);
+		if( numBytes < 0 ) {
+	    	sl_Close(iSockID);
+	    	ASSERT_ON_ERROR(SEND_ERROR);
+	    }
+	    numBytesTotal += numBytes;
+	}
+
+/*
+    // Get picSize
+    numBytes = sl_Recv(iNewSockID, imageBuf, BUF_SIZE, 0);
+    if( numBytes <= 0 ) {
+    	sl_Close(iNewSockID);
+    	ASSERT_ON_ERROR(RECV_ERROR);
+    }
+    picSize = *(int*)imageBuf;
+
+    // Recv pic into buffer
+    while(numBytesTotal != picSize) {
+    	numBytes = sl_Recv(iNewSockID, &imageBuf[numBytesTotal],
+    			BUF_SIZE-numBytesTotal, 0);
+    	if( numBytes <= 0 ) {
+    	    sl_Close(iNewSockID);
+    	    ASSERT_ON_ERROR(RECV_ERROR);
+    	}
+    	numBytesTotal += numBytes;
+    }
+
+    // Send picSize
+    numBytes = sl_Send(iNewSockID, (void*)&picSize, sizeof(int), 0);
+    if( numBytes < 0 )
+    {
+        sl_Close(iSockID);
+        ASSERT_ON_ERROR(SEND_ERROR);
+    }
+
+    // Send pic
+    numBytesTotal = 0;
+    while(numBytesTotal != picSize) {
+    	numBytes = sl_Send(iNewSockID, &imageBuf[numBytesTotal],
+    			picSize-numBytesTotal, 0);
+    	if( numBytes < 0 )
+    	{
+    		sl_Close(iSockID);
+    	    ASSERT_ON_ERROR(SEND_ERROR);
+    	}
+    	numBytesTotal += numBytes;
+    }
+
+    // Recieve string
+    numBytes = sl_Recv(iNewSockID, imageBuf, BUF_SIZE, 0);
+    if( numBytes <= 0 ) {
+    	sl_Close(iNewSockID);
+    	ASSERT_ON_ERROR(RECV_ERROR);
+    }
+
+    // Send back string
+    numBytes = sl_Send(iNewSockID, imageBuf, numBytes, 0);
+    if( numBytes <= 0 ) {
+    	sl_Close(iNewSockID);
+    	ASSERT_ON_ERROR(SEND_ERROR);
+    }
+*/
+
+    // close the connected socket after receiving from connected TCP client
+    numBytes = sl_Close(iNewSockID);
+    ASSERT_ON_ERROR(numBytes);
+    // close the listening socket
+    numBytes = sl_Close(iSockID);
+    ASSERT_ON_ERROR(numBytes);
+
+    while(1){};
+    return SUCCESS;
+}
+
 
 //****************************************************************************
 //
@@ -183,8 +385,6 @@ void WebSocketHandshakeEventHandler(UINT16 uConnection)
 //! plug
 //!
 //****************************************************************************
-
-
 void HttpServerAppTask(void * param)
 {
 	long lRetVal = -1;
@@ -197,19 +397,7 @@ void HttpServerAppTask(void * param)
         LOOP_FOREVER();
     }	
 
-	//Stop Internal HTTP Server
-	lRetVal = sl_NetAppStop(SL_NET_APP_HTTP_SERVER_ID);
-    if(lRetVal < 0)
-    {
-        ERR_PRINT(lRetVal);
-        LOOP_FOREVER();
-    }	
-
-	//Run APPS HTTP Server
-	HttpServerInitAndRun(NULL);
-
-	LOOP_FOREVER();
-
+    BsdTcpServer(5001);
 }
 
 //*****************************************************************************
