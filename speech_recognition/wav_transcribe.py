435e7d89fb250ae6d2bda24ad791416dd097e291
Zheng Hao Tan <tanzhao@umich.edu>
2015-11-13 17:00:47 -0500

    added speech recognition samples

---
+++ speech_recognition/wav_transcribe.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+# obtain path to "test.wav" in the same folder as this script
+from os import path
+WAV_FILE = path.join(path.dirname(path.realpath(__file__)), "salli.wav")
+
+# use "test.wav" as the audio source
+r = sr.Recognizer()
+with sr.WavFile(WAV_FILE) as source:
+    audio = r.record(source) # read the entire WAV file
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print(r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
