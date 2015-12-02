f89c690ebc432cb502074fe10a908b8cef97e7bb
Jconn <johnconn@umich.edu>
2015-12-01 19:33:20 -0500

    microphone impl
    added code for grabbing 'x' seconds of data from the microphone

--- modules/microphone/microphone.c
+++ modules/microphone/microphone.c
@@ -1,7 +1,66 @@
 #include "microphone.h"
+#include "adc.h"
+#include "rom_map.h"
+#include "hw_memmap.h"
+#include "../interrupts/timer_interrupt.h"
+#include <stdbool.h>
 
+
+//this function accepts a pointer to a buffer
+//and the number of seconds of recording is desired
+//and fills the buffer with microphone data
+//it returns the number of bytes of data stored in the buffer.
+//The current system is set up to record 10000 samples per second.
+//this corresponds to 20000 bytes per second
+// some common values of numSeconds
+// numSeconds = 1 -> 20000 bytes
+// numSeconds = 2 -> 40000 bytes
+// numSeconds = 3 -> 60000 bytes
+uint32_t GetAudio(char * buf, uint8_t numSeconds){
+
+	TimerConfigNStart(numSeconds*SAMPLE_RATE, buf);
+
+	while(!RecordingDone){
+	}
+	TimerDeinitStop();
+	return MIC_SAMPLE_SIZE*CurrentSamples;
+
+}
+
+//this function grabs an ADC sample
+inline uint16_t GetMicSample() {
+    /*
+    ADC returns 32 bits
+    [1:0] : Reserved
+	[13:2] : ADC sample Bits
+	[30:14]: time stamp per ADC sample
+	[31] : Reserved
+     */
+    //grab only the mic value
+	return ((MAP_ADCFIFORead(ADC_BASE, ADC_CH_0) >> 2) & 0xFFF);
+}
+
+
+
+
+//used to initialize the ADC associated with the microphone, must be called before
+//you do any mic stuff
 void InitializeMicrophone() {
 
+		// // Configure ADC timer which is used to timestamp the ADC data samples
+		//
+		MAP_ADCTimerConfig(ADC_BASE,2^17);
 
+		//
+		// Enable ADC timer which is used to timestamp the ADC data samples
+		//
+		MAP_ADCTimerEnable(ADC_BASE);
 
+	 	// Enable ADC module
+		MAP_ADCEnable(ADC_BASE);
+
+		//
+		// Enable ADC channel
+		//
+		MAP_ADCChannelEnable(ADC_BASE, ADC_CH_0);
 }
